9d85d882d972788d72953424527881956acc2587
FATIMA QURESHI <115686917+Fatimaq07@users.noreply.github.com>
2025-11-18 19:53:01 +0530

    Refactor route handling for static files

--- server/server.js
+++ server/server.js
@@ -39,10 +39,11 @@ if (process.env.NODE_ENV === "production") {
 
   app.use(express.static(path.join(__dirname, "../client/dist")));
 
-  app.get("/*", (req, res) => {
+  app.use((req, res) => {
     res.sendFile(path.join(__dirname, "../client/dist/index.html"));
   });
 }
+
 
 // Start server
 app.listen(PORT, () => {
